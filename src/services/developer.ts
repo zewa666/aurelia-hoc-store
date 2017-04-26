47e5eff615d0354564b6d06fb6c5452b247c9150
Vildan Softic <zewa666@gmail.com>
2017-04-26 18:56:02 +0200

    add devtools and state syncing support

--- src/services/developer.ts
+++ src/services/developer.ts
@@ -2,7 +2,10 @@ import {
   BehaviorSubject,
   Observable
 } from 'rxjs';
-import { autoinject } from 'aurelia-framework';
+import { 
+  autoinject,
+  LogManager
+} from 'aurelia-framework';
 
 export interface Developer {
   name: string;
@@ -70,13 +73,23 @@ export interface DeveloperState {
 
 @autoinject()
 export class DeveloperStore {
+  // Aurelia logging helper
+  public logger = LogManager.getLogger("DeveloperStore");
+
+  // Redux-DevTools? Hell yeah
+  public devToolsAvailable: boolean = false;
+  public devTools: any;
+
+  // our initial state
+  public initialState: DeveloperState = {
+    developers: [],
+    activeCategory: null
+  };
+
   // model your overall state as a BehaviorSubject(BHS) to be able to get the recent value
   // make sure the BHS is not exposed, so that from outside nobody is able to change the state
   // you can also specify the initial state (e.g. from localStorage) by passing it in as an attribute
-  private _state: BehaviorSubject<DeveloperState> = new BehaviorSubject({
-    developers: [],
-    activeCategory: null
-  });
+  private _state: BehaviorSubject<DeveloperState> = new BehaviorSubject(this.initialState);
 
   // this observable is the only access for outside consumers, thus creating a single point of truth
   // it will propagate all changes made to the BHS
@@ -85,6 +98,7 @@ export class DeveloperStore {
   // extract implementations into a simple service
   // this way you can leverage both a observable and traditional style
   constructor(private developerService: DeveloperService) {
+    this.setupDevTools();
     this.loadAllDevs();
   }
 
@@ -111,6 +125,9 @@ export class DeveloperStore {
 
         // promote changes to other subscribers
         this._state.next(state);
+
+        // update devtools
+        this.updateDevToolsState("add new developer", state);
       }
     );
 
@@ -146,8 +163,42 @@ export class DeveloperStore {
 
         // and notify subscribers about changes
         this._state.next(state);
+
+        // update devtools
+        this.updateDevToolsState(`load ${category} devs`, state);
       },
       err => console.log(`Error loading ${category} developers`)
     );
+  }
+
+  private setupDevTools() {
+    // check whether the user has the Redux-DevTools browser extension installed
+    if ((<any>window).devToolsExtension) {
+      this.logger.info("DevTools are available");
+      this.devToolsAvailable = true;
+
+      // establish a connection with the DevTools
+      this.devTools = (<any>window).__REDUX_DEVTOOLS_EXTENSION__.connect();
+
+      // set the initial state
+      this.devTools.init(this.initialState);
+
+      // subscribe to changes, e.g navigation from within the DevTools
+      this.devTools.subscribe((message: any) => {
+        this.logger.debug(`DevTools sent change ${message.type}`);
+
+        if (message.type === "DISPATCH") {
+          // the state is sent as string, so don't forget to parse it :)
+          this._state.next(JSON.parse(message.state));
+        }
+      });
+    }
+  }
+
+  private updateDevToolsState(action: string, state: DeveloperState) {
+    // if the Redux-DevTools are available, sync the states
+    if (this.devToolsAvailable) {
+      this.devTools.send(action, state);
+    }
   }
 }
